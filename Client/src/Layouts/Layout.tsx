f60a1e0b1b6debbe24c458a111917b5880516d32
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-06-18 14:16:15 +0545

    new layout added

---
+++ Client/src/Layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar/Navbar";
+import MainContent from "./MainContent/MainContent";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <MainContent />
+      
+    </>
+  );
+}
+
+export default Layout;
