5e189785245855059b1ee161a98e4c6ac81cae1e
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-06-19 17:13:46 +0545

    image and new componet added

---
+++ Client/src/Components/Ui/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-[#8b5d3b] text-white px-6 py-2 rounded-full flex items-center gap-2 hover:bg-[#603912] transition cursor-pointer"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
