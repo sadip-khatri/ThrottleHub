ec77e0d94655877ca5382314711be150618a7f91
bhaire7 <baibhavnepal07@gmail.com>
2025-06-23 17:35:54 +0545

    Add cart

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -13,6 +13,8 @@ import Layout from "./Layouts/Layout";
 import Home from "./Pages/Home/Home";
 
 import CartPage from "./Pages/Cart/CartPage";
+import ProductDetail from "./Pages/ProductDetails/ProductDetails";
+import CartTry from "./Pages/Cart/CartTry";
 
 // import Home from "./Pages/Home/Hero";
 
@@ -32,11 +34,9 @@ function App() {
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<Profile />} />
           <Route path="login" element={<LogIn />} />
-
           <Route path="cart" element={<CartPage />} />
-
-          <Route path="/product/:id" element={<ProductDetails />} />
-
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cartTry" element={<CartTry />} />
         </Route>
       </Routes>
     </Router>
