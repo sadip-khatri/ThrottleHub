faea7c0852d78306713c3f9cc3b5451cccf62441
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-06-27 16:51:01 +0545

    creating the blog section

---
+++ Client/src/Pages/Blogs/Blogs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TopHead from "../../Components/Shared/Blogs/TopHead";
+
+function Blogs() {
+  return (
+    <>
+      <TopHead />
+    </>
+  );
+}
+
+export default Blogs;
