afca4bc782cda8aabf0ba38823e10e8bd4bb9670
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-06-30 16:07:13 +0545

    changes in similar products

--- Client/src/Pages/ProductDetails/ProductDetails.tsx
+++ Client/src/Pages/ProductDetails/ProductDetails.tsx
@@ -2,8 +2,10 @@
 // src/Pages/ProductDetail.tsx
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import api from "../../utils/api";
+import api from "../../Utils/api";
 import ProductInfo from "../../Components/Ui/ProductInfo";
+// import SimilarItems from "../../Components/Shared/SimilarItems/SimilarItems";
+import YouMightAlsoLike from "../../Components/Shared/YouMIghtAlsoLike/YouMightAlsoLike";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -30,31 +32,37 @@ const ProductDetail = () => {
   if (!product) return <p className="text-center mt-20">Product not found.</p>;
 
   const handleAddToCart = async (
-  selectedProduct: any,
-  selectedSize: string = "M",
-  quantity: number = 1
-) => {
-  try {
-    await api.post("/cart", {
-      productId: selectedProduct._id,
-      size: selectedSize,
-      quantity,
-    });
+    selectedProduct: any,
+    selectedSize: string = "M",
+    quantity: number = 1
+  ) => {
+    try {
+      await api.post("/cart", {
+        productId: selectedProduct._id,
+        size: selectedSize,
+        quantity,
+      });
 
-    alert(`${selectedProduct.title} added to cart!`);
-  } catch (err) {
-    console.error("Failed to add to cart", err);
-    alert("Failed to add to cart.");
-  }
-};
-
+      alert(`${selectedProduct.title} added to cart!`);
+    } catch (err) {
+      console.error("Failed to add to cart", err);
+      alert("Failed to add to cart.");
+    }
+  };
 
   return (
-    <div className="max-w-6xl mx-auto p-4 grid grid-cols-1 md:grid-cols-2 gap-10">
-    
-      {/* <ProductGallery product={product} /> */}
-      <ProductInfo product={product} onAddToCart={handleAddToCart} currencyCode={""} />
-    </div>
+    <>
+      <div className="max-w-6xl mx-auto p-4 grid-cols-1 md:grid-cols-2 gap-10">
+        {/* <ProductGallery product={product} /> */}
+        <ProductInfo
+          product={product}
+          onAddToCart={handleAddToCart}
+          currencyCode={""}
+        />
+      </div>
+
+      <YouMightAlsoLike />
+    </>
   );
 };
 
