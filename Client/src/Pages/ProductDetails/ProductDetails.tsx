2fe021db5ea7b1134e77d41d7f3be241223cf6a0
sadipkhatri21 <sadipkhatri123@gmail.com>
2025-06-30 16:19:38 +0545

    push to change

--- Client/src/Pages/ProductDetails/ProductDetails.tsx
+++ Client/src/Pages/ProductDetails/ProductDetails.tsx
@@ -52,7 +52,7 @@ const ProductDetail = () => {
 
   return (
     <>
-      <div className="max-w-6xl mx-auto p-4 grid-cols-1 md:grid-cols-2 gap-10">
+      <div className="mx-auto p-4 grid-cols-1 md:grid-cols-2 gap-10">
         {/* <ProductGallery product={product} /> */}
         <ProductInfo
           product={product}
